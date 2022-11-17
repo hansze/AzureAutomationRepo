ce73d79528ef31cbb65472c36b783942f6231a0f
hansze <hansze@microsoft.com>
2022-11-17 13:43:01 +0100

    Again

---
+++ Modules/AADemo/AADemo.psm1
@@ -0,0 +1,3 @@
+
+function Get-Coffee {param() Write-Host 'Here is your coffee' -ForegroundColor Green}
+function Set-Coffee {param() Write-Host 'Adding sugar to  your coffee' -ForegroundColor Green}
